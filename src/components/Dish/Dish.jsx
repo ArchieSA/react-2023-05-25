7da4dcc44a82754f08bff8ac185c55e22d7acac9
alextsyg <innempaya@gmail.com>
2023-06-11 21:23:59 +0300

    hw2: 1-3

--- src/components/Dish/Dish.jsx
+++ src/components/Dish/Dish.jsx
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Dish = ({ dish }) => {
   if (!dish) {
     return null;
   }
 
-  const { name, price } = dish;
+  const [quantity, setQuantity] = useState(0);
+
+  const { name, price, ingredients } = dish;
+
+  const incrementDish = () => {
+    if (quantity >= 0 && quantity < 5) {
+      setQuantity((quantity) => quantity + 1);
+    }
+  };
+  const decrementDish = () => {
+    if (quantity > 0 && quantity <= 5) {
+      setQuantity((quantity) => quantity - 1);
+    }
+  };
+
   return (
     <div>
       <p>{name}</p>
       <p>{price}</p>
+      <button onClick={decrementDish}>-</button>
+      <span>{quantity}</span>
+      <button onClick={incrementDish}>+</button>
+      {quantity ? (
+        <ul>
+          {ingredients.map((ingredient) => (
+            <li>{ingredient}</li>
+          ))}
+        </ul>
+      ) : null}
     </div>
   );
 };
--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
+const ratingArray = [1, 2, 3, 4, 5];
+
 export const Rating = ({ value, onChange }) => {
-  return <div>Rating</div>;
+  return (
+    <div>
+      {ratingArray.map((ratingValue) => (
+        <button
+          disabled={ratingValue === value}
+          onClick={() => onChange(ratingValue)}
+        >
+          {ratingValue}
+        </button>
+      ))}
+    </div>
+  );
 };

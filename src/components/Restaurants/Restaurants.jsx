e5b5daacbed1ffb8805818ae8fa10a17d3739873
Ilya Govorkov <i.govorkov@qiwi.com>
2023-06-11 19:19:40 +0300

    Additional task-2 done

--- src/components/Restaurants/Restaurants.jsx
+++ src/components/Restaurants/Restaurants.jsx
@@ -4,12 +4,38 @@
 import { Restaurant } from "@/components/Restaurant/Restaurant";
 import React, { useEffect, useState } from "react";
 
+
+
 export const Restaurants = ({ restaurants }) => {
   let [activeRestaurantIndex, setActiveRestaurantIndex] = useState(0);
 
-  useEffect(() => {
-    console.log("reset");
-  }, [activeRestaurantIndex]);
+  let [searchText, setSearchText] = useState("");
+
+  let [searchTimeout, setSearchTimeout] = useState(null);
+
+  const searchRestaurant = (searchText) => {
+
+    if(!searchText?.length)
+      return;
+       
+    let index = restaurants.map((({name})=> name.toLowerCase())).findIndex((name)=> name.startsWith(searchText.toLowerCase()));
+
+    setActiveRestaurantIndex(index != -1? index : activeRestaurantIndex);
+  };
+
+
+  useEffect(()=>{  
+    if(searchTimeout !== null) 
+      clearTimeout(searchTimeout);       
+    
+    setSearchTimeout(setTimeout(()=> { searchRestaurant(searchText);}, 2000))
+
+  },
+  [searchText]);
+
+  // useEffect(() => {
+  //   console.log("reset");
+  // }, [activeRestaurantIndex]);
 
   return (
     <div>
@@ -23,6 +49,9 @@ export const Restaurants = ({ restaurants }) => {
             {name}
           </button>
         ))}
+      </div>
+      <div>
+        <input type="text" value={searchText} onChange={(event) => setSearchText(event.target.value)}/>
       </div>
       <Restaurant restaurant={restaurants[activeRestaurantIndex]} />
     </div>

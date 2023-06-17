d16a1801c43182f177e3b1c4bbb3fa24cae544cd
Victor Stepanov <vix522430@bk.ru>
2023-06-17 15:10:41 +0300

    finished hw4

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,23 @@
+'use client';
+import { useVersion } from '@/contexts/version';
 import cn from 'classnames';
+import { Button } from '../Button/Button';
 import styles from './styles.module.scss';
 
 export const Footer = ({ classNames }) => {
-  return <footer className={cn(styles.root, classNames)}>Footer</footer>;
+  const { version, setVersion } = useVersion();
+
+  const versionName = {
+    desktop: 'Десктопная',
+    mobile: 'Мобильная',
+  };
+
+  return (
+    <footer className={cn(styles.root, classNames)}>
+      <Button
+        viewVariant={'switcher'}
+        onClick={setVersion}
+      >{`${versionName[version]} версия`}</Button>
+    </footer>
+  );
 };

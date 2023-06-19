d745bbd170c55a9a2afa56b4e70acac0d1185625
Ilya Govorkov <i.govorkov@qiwi.com>
2023-06-19 17:41:58 +0300

    Implement custom useCount hook.
    Task 3 done.

--- src/components/Dish/Dish.jsx
+++ src/components/Dish/Dish.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/jsx-key */
-import React, { useState } from "react";
+import React from "react";
 
 import styles from "./styles.module.scss";
 import { Button } from "@/components/Button/Button";
+import { useCount } from "@/hooks/useCount";
 
 export const Dish = ({ dish }) => {
-  const [count, setCount] = useState(0);
+  const [count, increaseDishCount, decreaseDishCount] = useCount(0);
 
   if (!dish) {
     return null;
@@ -19,7 +20,7 @@ export const Dish = ({ dish }) => {
       <p>{price}</p>
       <div>
         <Button
-          onClick={() => setCount(count - 1)}
+          onClick={decreaseDishCount}
           disabled={count === 0}
           className={styles.action}
         >
@@ -27,7 +28,7 @@ export const Dish = ({ dish }) => {
         </Button>
         {count}
         <Button
-          onClick={() => setCount(count + 1)}
+          onClick={increaseDishCount}
           disabled={count === 5}
           className={styles.action}
           viewVariant="secondary"

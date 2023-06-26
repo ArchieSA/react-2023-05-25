7663128fa3dc486cb57c6a19337acd0b9d7bce59
Maksim Chizhov <machizhov@avito.ru>
2023-06-26 14:08:04 +0300

    forgot users

--- src/containers/MenuContainer.jsx
+++ src/containers/MenuContainer.jsx
@@ -1,9 +1,8 @@
 import { Menu } from "@/components/Menu/Menu";
 import { selectDishIsLoading } from "@/redux/features/dish/selectors";
-import { fetchDishesIfNotExist } from "@/redux/features/dish/thunks/fetchDishesifNotExist";
 import { selectRestaurantDishIds } from "@/redux/features/restaurant/selectors";
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 
 export const MenuContainer = ({ restaurantId, className }) => {
   const dishIds = useSelector((state) =>
--- src/containers/UserContainer.jsx
+++ src/containers/UserContainer.jsx
@@ -1,10 +1,16 @@
 import { User } from "@/components/User/User";
-import { selectUser } from "@/redux/features/user/selectors";
+import { selectUser, selectUsersAreLoading } from "@/redux/features/user/selectors";
 import React from "react";
 import { useSelector } from "react-redux";
 
 export const UserContainer = ({ userId, ...props }) => {
   const user = useSelector((state) => selectUser(state, userId));
+
+  const status = useSelector((state) => selectUsersAreLoading(state));
+
+  if (status) {
+    return <div>User is loading, wait please...</div>
+  }
 
   if (!user) {
     return null;
